c8d52b3bf50d3f9d2193c6896fa0f864f6546c5f
littleviil <netlena30@gmail.com>
2025-03-23 20:02:35 +0300

    Edits

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,14 @@
 import { useState, useEffect } from 'react';
-import { PopExit } from './compoents/popus/PopExit/PopExit'
-import { PopNewCard } from './compoents/popus/PopNewCard/PopNewCard'
-import { PopBrowse } from './compoents/popus/PopBrowse/PopBrowse'
-import { Header } from './compoents/Header/Header'
-import { Main } from './compoents/Main/Main'
-import './App.css'
+import { PopExit } from './compoents/popus/PopExit/PopExit';
+import { PopNewCard } from './compoents/popus/PopNewCard/PopNewCard';
+import { PopBrowse } from './compoents/popus/PopBrowse/PopBrowse';
+import { PopUser } from './compoents/popus/PopUser/PopUser';
+import { Header } from './compoents/Header/Header';
+import { Main } from './compoents/Main/Main';
+import './App.css';
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [isPopUserOpen, setIsPopUserOpen] = useState(false);
   const [isPopExitOpen, setIsPopExitOpen] = useState(false);
   const [isPopNewCardOpen, setIsPopNewCardOpen] = useState(false);
   const [isPopBrowseOpen, setIsPopBrowseOpen] = useState(false);
@@ -20,29 +20,18 @@ function App() {
   }, []);
 
   return (
-    <>
-      <div className="wrapper">	
-        {isPopExitOpen && <PopExit onClose={() => setIsPopExitOpen(false)} />}
-        {isPopNewCardOpen && <PopNewCard onClose={() => setIsPopNewCardOpen(false)} />}
-        {isPopBrowseOpen && <PopBrowse onClose={() => setIsPopBrowseOpen(false)} />}
-        {isPopUserOpen && <PopUser onClose={() => setIsPopUserOpen(false)} />}
-		    <PopExit />
-		    <PopNewCard />
-		    <PopBrowse />
-		    <Header 
-          onUserClick={() => setIsPopUserOpen(true)}
-          onExitClick={() => setIsPopExitOpen(true)}
-          onNewCardClick={() => setIsPopNewCardOpen(true)}
-          onBrowseClick={() => setIsPopBrowseOpen(true)}
-        />
-		    {loading ? (
-            <div className="loading">Данные загружаются...</div>
-          ) : (
-            <Main />
-          )}
-      </div>
-    </>
-  )
+    <div className="wrapper">
+      {isPopExitOpen && <PopExit onClose={() => setIsPopExitOpen(false)} />}
+      {isPopNewCardOpen && <PopNewCard onClose={() => setIsPopNewCardOpen(false)} />}
+      {isPopBrowseOpen && <PopBrowse onClose={() => setIsPopBrowseOpen(false)} />}
+      <Header onNewCardClick={() => setIsPopNewCardOpen(true)} />
+      {loading ? (
+        <div className="loading">Данные загружаются...</div>
+      ) : (
+        <Main />
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
